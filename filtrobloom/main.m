3f6f572819b3f1d60760655a0376201716b262e1
JPSP9547 <joaopp@ua.pt>
2024-12-11 19:19:41 +0000

    Updated Filtro Bloom

--- filtrobloom/main.m
+++ filtrobloom/main.m
@@ -4,24 +4,28 @@ clc
 dic = readcell("save/reduced.csv");
 userNames = dic(:, 1);
 
-m = length(userNames);
-n = length(userNames) * 100;
+m = length(userNames); %quantos elementos vao ser adicionados
+n = length(userNames) * 100; % tamanho do filtro
 
-k = floor(n*log10(2) / m);
-filtroBloomUsers = filtroBloomString(n, k);
+k = floor(n*log10(2) / m); % calcular k otimo (k = funcoes de hash que vao ser usadas)
+filtroBloomUsers = filtroBloomString(n, k); %criar filtro bloom
 
-counter = 1;
+userRepeated = 0; %variavel que guarda quantos nomes repetidos
+invalidUsers = 0; %variavel que guarda quantas linhas são "invalidas" (serve mais para debug e testes)
 for i = 1:m
     
     name = userNames{i};
     
-    if isa(name, 'missing')
-        continue
-    end
+    if isa(name, 'missing') %no dataset pode haver linhas sem user name
+        invalidUsers = invalidUsers + 1;
+        continue            %entao este if faz com que o programa nao "morra"
+    end                     %caso haja uma linha sem username
 
-    if(filtroBloomUsers.checkElement(name) == 1)
-        counter = counter + 1;
+    if(filtroBloomUsers.checkElement(name) == 1)  %checa se o userName já está no filtro
+        userRepeated = userRepeated + 1;
     else
-        filtroBloomUsers = filtroBloomUsers.addElement(name);
+        filtroBloomUsers = filtroBloomUsers.addElement(name); %adiciona o userName ao filtro
     end
 end
+
+userRepeated + invalidUsers + sum(filtroBloomUsers.hashTable) %tem que dar igual a m
