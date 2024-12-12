808673e5df1597ce01e56d83a7031fc7c5957e3a
JPSP9547 <joaopp@ua.pt>
2024-12-12 08:35:58 +0000

    Changes to filtro bloom

---
+++ filtrobloom/hashFunction2.m
@@ -0,0 +1,10 @@
+function hc = hashFunction2(elemento,hf)
+
+size = length(elemento);
+
+for i = 1:hf
+    elemento(size + i) =  num2str(mod(i, 10));
+end
+
+hc = string2hash(elemento);
+
--- filtrobloom/main.m
+++ filtrobloom/main.m
@@ -11,21 +11,20 @@ k = floor(n*log10(2) / m); % calcular k otimo (k = funcoes de hash que vao ser u
 filtroBloomUsers = filtroBloomString(n, k); %criar filtro bloom
 
 userRepeated = 0; %variavel que guarda quantos nomes repetidos
-invalidUsers = 0; %variavel que guarda quantas linhas são "invalidas" (serve mais para debug e testes)
+counter = 1;
 for i = 1:m
     
     name = userNames{i};
     
     if isa(name, 'missing') %no dataset pode haver linhas sem user name
-        invalidUsers = invalidUsers + 1;
         continue            %entao este if faz com que o programa nao "morra"
     end                     %caso haja uma linha sem username
 
     if(filtroBloomUsers.checkElement(name) == 1)  %checa se o userName já está no filtro
         userRepeated = userRepeated + 1;
+        repeatedNames{counter} = name;
+        counter = counter + 1;
     else
         filtroBloomUsers = filtroBloomUsers.addElement(name); %adiciona o userName ao filtro
     end
 end
-
-userRepeated + invalidUsers + sum(filtroBloomUsers.hashTable) %tem que dar igual a m
