961660a941f972633732d64d5da01f23a1f2379c
JPSP9547 <joaopp@ua.pt>
2024-12-13 00:40:22 +0000

    get better hash

---
+++ filtrobloom/getNPrimeNumbers.m
@@ -0,0 +1,16 @@
+function vectorPrime = getNPrimeNumbers(start, n)
+%getNPrimeNumbers   returns a 1xN vector with N prime numbers starting on
+%start
+
+number = start;
+vectorPrime = zeros(1, n);
+counter = 1;
+
+while(counter < n)
+    if isprime(number)
+        vectorPrime(counter) = number;
+        counter = counter + 1;
+    else
+        number = number + 1;
+    end
+end
