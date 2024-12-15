fe9748b4bf19ab536ebca4cbde20170424a30d43
JPSP9547 <joaopp@ua.pt>
2024-12-15 03:09:43 +0000

    added bloom filter support

--- sistema_analise_comentarios/main_com_interface.m
+++ sistema_analise_comentarios/main_com_interface.m
@@ -17,22 +17,35 @@ height = 600;
 
 window = uifigure('Name', 'Sistema de analise de comentarios', ...
     'Position', [500, 50, width, height]);
+tabGroup = uitabgroup(window,"Position", [0, 0, width, height]);
+tab1 = uitab(tabGroup, "Title", "Comentários");
+tab2 = uitab(tabGroup, "Title", "Filtrar utilizadores");
 
-search_bar = uieditfield(window,'text','Position',[5 560 width-5 30 ]);
-threshold_slider = uislider(window,'Position',[50 530 90 3], ...
+%% first tab
+search_bar = uieditfield(tab1,'text','Position',[5 560 width-5 30 ]);
+threshold_slider = uislider(tab1,'Position',[50 530 90 3], ...
     'Limits', [0, 1], 'Value', 0.5,'MajorTicks', [],'MinorTicks',[]);
-threshold_label = uilabel(window, 'Position', [150 515 100 30], ...
+threshold_label = uilabel(tab1, 'Position', [150 515 100 30], ...
     'Text', ['semelhança: ', num2str(threshold_slider.Value)]);
-naivebayes_label = uilabel(window, 'Position', [300 515 400 30], ...
+naivebayes_label = uilabel(tab1, 'Position', [300 515 400 30], ...
     'Text', 'Naive Bayes: selecione um comentário','FontSize', 16);
 
-output = uilistbox(window, 'Position', [25, 25, width-50, height-(height-515+25)], ...
+output = uilistbox(tab1, 'Position', [25, 25, width-50, height-(height-515+25)], ...
     'Multiselect', 'off','FontSize', 14, ...
     'ValueChangedFcn', @(src, event) ...
     naiveBayesCalculation(src,naivebayes_label,prior,vocabulary,loglikelihood,classes,minSize));
 
 output.Items = {};
 
+%% second tab
+limiarLabel = uilabel(tab2, 'Position', [10 height-80 400 30], ...
+    'Text', 'Quantos comentários no minimo tem o utilizador?','FontSize', 14);
+limiarInput = uieditfield(tab2,'numeric','Position',[350 height-80 100 30 ]);
+usersLabel = uilabel(tab2, 'Position', [10 height-120 400 30], ...
+    'Text', ['Usuaŕios com menos de ', num2str(limiarInput.Value)],'FontSize', 14);
+outputFiltro = uilistbox(tab2, 'Position', [25, 25, width-50, height-(height-515+25)]);
+outputFiltro.Items = {};
+%% first tab backend
 % when a list element is selected calculate the naive bayes prediction to
 % the review
 function naiveBayesCalculation(selected,naivebayes_label,prior,vocabulary,loglikelihood,classes,minSize)
@@ -121,4 +134,32 @@ function updateThreshold(slider,label)
     % update label value
     threshold = slider.Value;
     label.Text = ['threshold: ', num2str(threshold)];
+end
+
+%% second tab backend
+%when the limiar changes
+limiarInput.ValueChangedFcn = @(src, event) updateLimiar(limiarInput, counting_bloom_filter, users, outputFiltro);
+function updateLimiar(limiarInput, counting_bloom_filter, users, outputFiltro)
+    limiar = limiarInput.Value;
+    
+    repeatedUsersCounter = 0;
+    repeatedUsers = cell(0);
+    for i = 1:counting_bloom_filter.nmrOfElements
+
+        user = users{i};
+
+        if isa(user, 'missing') %no dataset pode haver linhas sem user name
+            continue            %entao este if faz com que o programa nao "morra"
+        end                     %caso haja uma linha sem username
+    
+        if isa(user, "double")
+            user = convertStringsToChars(int2str(user));
+        end
+
+        if( counting_bloom_filter.isRepeatedLessThan(user, limiar) == 1 && (ismember(user, repeatedUsers) == false)) %nao contar mais que uma vez os repetidos
+            repeatedUsersCounter = repeatedUsersCounter + 1;
+            repeatedUsers{repeatedUsersCounter} = user;
+        end
+    end
+    outputFiltro.Items = repeatedUsers;
 end
