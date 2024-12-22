00ba2d08c9043c509d1a6a737e29e283909ff9d3
thiago.vicente@ua.pt <thiago.vicente@ua.pt>
2024-12-22 20:02:32 +0000

    sn

--- coutingBloomFilter/COUNTINGBF_genData.m
+++
@@ -1,4 +0,0 @@
-dic = readcell("save/reduced.csv");
-userNames = dic(:,1);
-
-save("save/output.mat", "userNames", '-mat');
--- coutingBloomFilter/hashFunctions2.m
+++
@@ -1,9 +0,0 @@
-function hc = hashFunctions2(elemento,hf)
-
-size = length(elemento);
-
-for i = 1:hf
-    elemento(size + i) =  num2str(mod(i, 10));
-end
-
-hc = string2hash(elemento);
